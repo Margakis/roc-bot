0898c406843bf4c50e6c9aefcc0506082fa2df07
Jens <32913789+Jens0512@users.noreply.github.com>
2017-11-08 20:55:31 +0100

    fix

--- Ship.js
+++ Ship.js
@@ -1,9 +1,9 @@
 class Ship {
 
   constructor(namee, weeapon, auura, zeen) {
-    this.name: string(namee);
-    this.weapon: string(weeapon);
-    this.aura: string(auura);
-    this.zen: string(zeen);
+    this.name = string(namee);
+    this.weapon = string(weeapon);
+    this.aura = string(auura);
+    this.zen = string(zeen);
   }
 }
