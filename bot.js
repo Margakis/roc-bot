3175b8467d0029ccc65994cdf124a7b07c578c8b
Jens <32913789+Jens0512@users.noreply.github.com>
2017-11-08 19:41:10 +0100

    test

--- bot.js
+++ bot.js
@@ -6,17 +6,20 @@ logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
+
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: process.env.TOKEN,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -30,7 +33,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'ships':
                 bot.sendMessage({
                     to: channelID,
-                    message: '<Loads of ships!!!>'
+                    message: '<Loads of ships!!!>\n<Even more ships!!!>'
                 });
             break;
             // Just add any case commands if you want to..
