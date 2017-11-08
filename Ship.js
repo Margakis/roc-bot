551f306251d2af1597a1c9946f6b712cc0263bec
Jens <32913789+Jens0512@users.noreply.github.com>
2017-11-08 20:43:37 +0100

    test

--- Ship.js
+++ Ship.js
@@ -1,10 +1,9 @@
 class Ship {
-  const name, weapon, aura, zen;
 
   constructor(name, weapon, aura, zen) {
-    this.name = string(name);
-    this.weapon = string(weapon);
-    this.aura = string(aura);
-    this.zen = string(zen);
+    name: string(name);
+    weapon: string(weapon);
+    aura: string(aura);
+    zen: string(zen);
   }
 }
