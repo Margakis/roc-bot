726d17874277103a979dc33f031e022d25562a1f
Jens <32913789+Jens0512@users.noreply.github.com>
2017-11-09 22:53:01 +0100

    fixes

--- Ship.js
+++ Ship.js
@@ -8,12 +8,17 @@ class Ship{
     this.zen = String(zen);
   }
   toString(){
-    var list = "";
-    for (var ship in file.ships) {
-      list += "\n" + ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
-    }
-    return string;
+    return ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
   }
 }
 
+function allships() {
+  var list = "";
+  for (var ship in file.ships) {
+    list += "\n" + ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
+  }
+  return string;
+}
+
 module.exports = Ship;
+module.exports = allships;
