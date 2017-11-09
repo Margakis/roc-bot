8736f98f74ec7b7ec61b884f8d9ebcee77b564f6
Jens0512 <32913789+Jens0512@users.noreply.github.com>
2017-11-09 13:19:30 +0100

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -1,5 +1,5 @@
 class Ship{
-  function Ship(name, weapon, aura, zen) {
+  constructor(name, weapon, aura, zen) {
     this.name = string(name);
     this.weapon = string(weapon);
     this.aura = string(aura);
