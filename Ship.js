2da90a4400a5d01ccce58df5fe1560f722298b0d
Jens0512 <32913789+Jens0512@users.noreply.github.com>
2017-11-10 12:31:34 +0100

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -1,16 +1,24 @@
-var file = require('./ships.json');
+module.exports {
+  var file = require('./ships.json');
 
-class Ship{
-  constructor(name, weapon, aura, zen) {
-    this.name = String(name);
-    this.weapon = String(weapon);
-    this.aura = String(aura);
-    this.zen = String(zen);
-  }
+  class Ship{
+    constructor(name, weapon, aura, zen) {
+      this.name = String(name);
+      this.weapon = String(weapon);
+      this.aura = String(aura);
+      this.zen = String(zen);
+    }
 
-  toString(){
-    return ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
+    toString(){
+      return ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
+    }
   }
+  
+  allships = function () {
+    var list = "List of all ship:\n";
+    for (var ship in allshipsf.ships) {
+      if(String(ship.name) != ''){
+        list += "\n" + ("**" + String(ship.name)+":** Weapon: *" + String(ship.weapon) +"*, Aura: *" + String(ship.aura) + "*, Zen: *" + String(ship.zen) + "*");
+      }
+    }
 }
-
-module.exports = Ship;
