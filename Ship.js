7c58afb6cf8abf59988cda6b4d867d1511d69e46
Jens0512 <32913789+Jens0512@users.noreply.github.com>
2017-11-10 12:59:19 +0100

    Update Ship.js

--- Ship.js
+++ Ship.js
@@ -1,24 +1,28 @@
 var file = require('./ships.json');
-module.exports = {
-  Ship: class{
-    constructor(name, weapon, aura, zen) {
-      this.name = String(name);
-      this.weapon = String(weapon);
-      this.aura = String(aura);
-      this.zen = String(zen);
-    };
+class Ship {
+  constructor(name, weapon, aura, zen) {
+    this.name = String(name);
+    this.weapon = String(weapon);
+    this.aura = String(aura);
+    this.zen = String(zen);
+  };
 
-    function tostring(){
-      return ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
-    }
-  },
-  
-  allships: function () {
-    var list = "List of all ship:\n";
-    for (var ship in allshipsf.ships) {
-      if(String(ship.name) != ''){
-        list += "\n" + ("**" + String(ship.name)+":** Weapon: *" + String(ship.weapon) +"*, Aura: *" + String(ship.aura) + "*, Zen: *" + String(ship.zen) + "*");
-      }
+  function tostring(){
+    return ("**" + this.name+":** Weapon: *" + this.weapon +"*, Aura: *" + this.aura + "*, Zen: *" + this.zen + "*");
+  }
+}
+
+function allships() {
+  var list = "List of all ship:\n";
+  for (var ship in allshipsf.ships) {
+    if(String(ship.name) != ''){
+      list += "\n" + ("**" + String(ship.name)+":** Weapon: *" + String(ship.weapon) +"*, Aura: *" + String(ship.aura) + "*, Zen: *" + String(ship.zen) + "*");
     }
   }
 }
+
+module.exports = {
+  Ship: Ship,
+  allships: allships
+}
+
